cdbf6a344076435f8a5ccaa0af8f9fd2d85fdc0a
Eddie <eddietseng@outlook.com>
2021-01-14 19:49:55 -0500

    Added in fade in library, added more height options to tail wind for vh.

--- components/about/AboutCard.jsx
+++ components/about/AboutCard.jsx
@@ -1,12 +1,16 @@
 /** @jsxRuntime classic /
 /** @jsx jsx */ jsx;
 import { jsx, css } from '@emotion/react';
+import Fade from "react-reveal/Fade";
+
 const AboutCard = ({name, title, description, imageUrl })=> (
-<div className="bg-gray-1 p-7 mb-4">
-  <h3 className="font-josefinSans">{name}</h3>
-  <h4 className="font-roboto">{title}</h4>
-  <p className="font-roboto">{description}</p>
-</div>
+  <Fade bottom duration={1000} distance="25px">
+    <div className="bg-gray-1 p-7 mb-4">
+      <h3 className="font-josefinSans">{name}</h3>
+      <h4 className="font-roboto">{title}</h4>
+      <p className="font-roboto">{description}</p>
+    </div>
+  </Fade>
 )
 
 export default AboutCard
--- pages/about.js
+++ pages/about.js
@@ -1,26 +1,29 @@
 import Layout from "../components/shared/Layout";
 import Image from "next/image";
 import AboutCard from "../components/about/AboutCard";
+import Fade from "react-reveal/Fade";
 
 const About = () => (
   <Layout>
     <div className="mx-auto w-12/13 md:w-10/13 lg:w-8/13 mb-10">
-      <div className="mt-10 lg:mt-36 lg:h-screen">
-        <div className="flex flex-col justify-center lg:flex-row">
-          <div className="mr-6 md:w-9/13 lg:w-7/13">
-            <h2 className="text-4xl mb-3 font-josefinSans">About Us</h2>
-            <p className="font-roboto">
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum."
-            </p>
+      <div className="my-10 lg:mb-36 lg:mt-24 lg:h-60v">
+        <Fade bottom duration={1000} distance="25px">
+          <div className="flex flex-col items-center justify-center lg:flex-row">
+            <div className="mr-6 md:w-9/13 lg:w-7/13">
+              <h2 className="text-4xl mb-10 font-josefinSans">About Us</h2>
+              <p className="font-roboto">
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum."
+              </p>
+            </div>
+            <Image src="/../public/temp-picture.png" width={600} height={458} />
           </div>
-          <Image src="/../public/temp-picture.png" width={500} height={381} />
-        </div>
+        </Fade>
       </div>
 
       <AboutCard

d159dd3e14628c14dd6f6a8b561da2325cd19a4b
Eddie <eddietseng@outlook.com>
2021-01-17 23:30:13 -0500

    Added image and imagePosition props to AboutCard

--- components/about/AboutCard.jsx
+++ components/about/AboutCard.jsx
@@ -1,16 +1,50 @@
 /** @jsxRuntime classic /
 /** @jsx jsx */ jsx;
-import { jsx, css } from '@emotion/react';
+import { jsx, css } from "@emotion/react";
 import Fade from "react-reveal/Fade";
+import Image from "next/image";
 
-const AboutCard = ({name, title, description, imageUrl })=> (
+const AboutCard = ({
+  name,
+  title,
+  description,
+  image,
+  imagePosition = "left",
+}) => (
   <Fade bottom duration={1000} distance="25px">
-    <div className="bg-gray-1 p-7 mb-4">
-      <h3 className="font-josefinSans">{name}</h3>
-      <h4 className="font-roboto">{title}</h4>
-      <p className="font-roboto">{description}</p>
+    <div className="flex flex-col bg-gray-1 mb-4 p-3 md:p-0 lg:p-0 md:bg-transparent lg:bg-transparent md:mx-0 lg:mx-0 md:flex-row lg:flex-row">
+      {imagePosition === "left" && image ? (
+        <div className=" mx-auto bg-gray-1 md:mr-4 lg:mr-4">
+          <Image
+            src="/../public/temp-picture.png"
+            width={250}
+            height={300}
+            layout="fixed"
+            objectFit="cover"
+            quality={100}
+          />
+        </div>
+      ) : null}
+
+      <div className="bg-gray-1 p-11 ">
+        <h3 className="font-josefinSans">{name}</h3>
+        <h4 className="font-roboto mb-3">{title}</h4>
+        <p className="font-roboto">{description}</p>
+      </div>
+
+      {imagePosition === "right" && image ? (
+        <div className=" mx-auto bg-gray-1 md:ml-4 lg:ml-4">
+          <Image
+            src="/../public/temp-picture.png"
+            width={250}
+            height={300}
+            layout="fixed"
+            objectFit="cover"
+          />
+        </div>
+      ) : null}
     </div>
   </Fade>
-)
+);
 
-export default AboutCard
+export default AboutCard;
--- pages/about.js
+++ pages/about.js
@@ -5,8 +5,8 @@ import Fade from "react-reveal/Fade";
 
 const About = () => (
   <Layout>
-    <div className="mx-auto w-12/13 md:w-10/13 lg:w-8/13 mb-10">
-      <div className="my-10 lg:mb-36 lg:mt-24 lg:h-60v">
+    <div className="  mb-10">
+      <div className=" mx-auto my-10 lg:mb-36 lg:mt-24 lg:h-60v w-12/13 md:w-10/13 lg:w-8/13">
         <Fade bottom duration={1000} distance="25px">
           <div className="flex flex-col items-center justify-center lg:flex-row">
             <div className="mr-6 md:w-9/13 lg:w-7/13">
@@ -26,30 +26,29 @@ const About = () => (
         </Fade>
       </div>
 
-      <AboutCard
-        name="Aoyama Kaede"
-        title="CEO/Senior designer"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat."
-      />
-      <AboutCard
-        name="Lee Xing"
-        title="Co-founder/COO"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat."
-      />
-      <AboutCard
-        name="Kenny Cao"
-        title="Renovation specialist"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat."
-      />
+      <div className="mx-auto w-12/13 md:w-11/13 lg:w-10/13">
+        <AboutCard
+          name="Aoyama Kaede"
+          title="CEO/Senior designer"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          image="test"
+          imagePosition="left"
+        />
+        <AboutCard
+          name="Lee Xing"
+          title="Co-founder/COO"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          image="test"
+          imagePosition="right"
+        />
+        <AboutCard
+          name="Kenny Cao"
+          title="Renovation specialist"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          image="test"
+          imagePosition="left"
+        />
+      </div>
     </div>
   </Layout>
 );

4f1dd7505d1fd1d8d165b2e2c444e99fd4d69504
Eddie <eddietseng@outlook.com>
2021-05-11 00:47:51 -0400

    Inputdropdown WIP

--- components/projects/InputDropdown.jsx
+++ components/projects/InputDropdown.jsx
@@ -4,21 +4,19 @@
 import React from "react";
 import { jsx, css } from "@emotion/react";
 
-const InputDropdown = ({ placeholder }) => {
+const InputDropdown = ({ label, options = [] }) => {
   return (
-    <select
-      id="cars"
-      name="cars"
-      className="w-48 bg-beige-1 border-white border-2 "
-    >
-      <option value="" disabled selected>
-        {placeholder}
-      </option>
-      <option value="volvo">Volvo</option>
-      <option value="saab">Saab</option>
-      <option value="fiat">Fiat</option>
-      <option value="audi">Audi</option>
-    </select>
+    <div>
+      <label htmlFor={label}>{label}</label>
+      <select name={label} className="w-48 bg-beige-1 border-white border-2 ">
+        <option value="" selected>
+          Any
+        </option>
+        {options.map((option) => (
+          <option value={option.value}>{option.label}</option>
+        ))}
+      </select>
+    </div>
   );
 };
 
--- pages/projects.js
+++ pages/projects.js
@@ -3,28 +3,36 @@ import ProjectCard from "../components/projects/ProjectCard";
 import Layout from "../components/shared/Layout";
 import useProjects from "../utils/useProjects";
 
+const bedroomOptions = [
+  {
+    label: "1+",
+    value: 1,
+  },
+  {
+    label: "1+",
+    value: 2,
+  },
+  {
+    label: "3+",
+    value: 3,
+  },
+];
+
 const Projects = () => {
   const { getProjects } = useProjects();
 
   const projects = getProjects();
-
-  // projects.push({ name: "test", description: "test" });
-  // projects.push({ name: "test", description: "test" });
-  // projects.push({ name: "test", description: "test" });
-  // projects.push({ name: "test", description: "test" });
-
-  console.log(projects);
 
   return (
     <Layout>
       <div className="flex-col bg-beige-1 px-5 py-10 w-full">
         <h2 className="text-4xl text-white mb-5">Find your Dream home</h2>
         <div className="flex-row flex justify-between">
-          <InputDropdown placeholder="Sort By" />
-          <InputDropdown placeholder="Status" />
-          <InputDropdown placeholder="Beds" />
-          <InputDropdown placeholder="Baths" />
-          <InputDropdown placeholder="Terms" />
+          <InputDropdown label="Sort By" />
+          <InputDropdown label="Status" />
+          <InputDropdown label="Beds" options={bedroomOptions} />
+          <InputDropdown label="Baths" />
+          <InputDropdown label="Terms" />
         </div>
       </div>
 
